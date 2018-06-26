b6caf3f2310f39959922f5c10c7d1ac326dac14a
Magda <magdalena.dusza13@gmail.com>
2018-06-26 22:23:47 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Cook Book!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          My new app
         </p>
       </div>
     );
